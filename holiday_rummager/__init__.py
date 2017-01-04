0463ec353d82614d3fc99a21f8def39d9c3049b5
Juan Manuel Torres Palma <j.m.torrespalma@gmail.com>
2017-01-04 00:14:04 +0000

    Create skeleton of project
    Organize and propare several files in the hierarchy of the source,
    including setup.py and the initial module directory.

---
+++ holiday_rummager/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2017 Juan Manuel Torres Palma <j.m.torrespalma@gmail.com>
+# This file is part of Holiday Rummager.
+#
+# Holiday Rummager is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# Holiday Rummager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Holiday Rummager.  If not, see <http://www.gnu.org/licenses/>
+
+"""
+holiday_rummager - Scrutinizes the web to find holidays.
+"""
+
+__author__ = "Juan Manuel Torres Palma  <j.m.torrespalma@gmail.com>"
+__copyright__ = "Copyright 2017, Juan Manuel Torres Palma"
+__license__ = "GPL"
+__version__ = "0.1"
+
+# Exported with *
+#__all__ = ( )
---
+++ hrmgr.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+import holiday_rummager
